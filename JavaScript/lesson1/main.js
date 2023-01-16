b0936a82985fcb8bce597931dab52a973d614cb7
MaksKozak <makskozak2400@gmail.com>
2023-01-16 15:18:54 +0200

    Initial commit

---
+++ JavaScript/lesson1/main.js
@@ -0,0 +1,99 @@
+let str = 'hello';
+console.log(str);
+alert(str)
+document.write('hello ');
+
+let str1 = 'owu';
+console.log(str1);
+alert(str1)
+document.write('owu ');
+
+let str2 = 'com';
+console.log(str2);
+alert(str2)
+document.write('com ');
+
+let str3 = 'ua';
+console.log(str3);
+alert(str3)
+document.write('ua ');
+
+let n1 = 1;
+console.log(n1);
+alert(n1)
+document.write('1 ');
+
+let n2 = 10;
+console.log(n2);
+alert(n2)
+document.write('10 ')
+
+let n3 = -999;
+console.log(n3);
+alert(n3)
+document.write('-999 ')
+
+let n4 = 123;
+console.log(n4);
+alert(n4)
+document.write('123 ')
+
+let n5 = 3.14;
+console.log(n5);
+alert(n5)
+document.write('3.14 ')
+
+let n6 = 2.7;
+console.log(n6);
+alert(n6)
+document.write('2.7 ')
+
+let n7 = 16;
+console.log(n7);
+alert(n7)
+document.write('16 ')
+
+let bool1 = true;
+console.log(bool1);
+alert(bool1)
+document.write('true ')
+
+let bool2 = false;
+console.log(bool2);
+alert(bool2)
+document.write('false ')
+
+
+
+
+
+let firstName = 'Kozak';
+console.log(firstName);
+
+let middleName = 'Maksym';
+console.log(middleName);
+
+let lastName = 'Tarasovych';
+console.log(lastName);
+
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+
+
+
+
+
+console.log(typeof 100);
+console.log(typeof '100');
+console.log(typeof true);
+
+
+
+
+let name = prompt('Enter name');
+let Maksym = prompt('Enter Maksym');
+let age = +prompt('Enter age');
+// console.log(name);
+// console.log(middleName);
+// console.log(age);
+
+
